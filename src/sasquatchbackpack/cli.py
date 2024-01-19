51209df71c24ed81e3c89e1a06210919f0404cac
Kai Koehler <kkoehler@lsst.org>
2024-01-19 15:53:20 -0700

    Add input Validation
    Add error message
    
    Update input validation
    
    Add success notation

--- src/sasquatchbackpack/cli.py
+++ src/sasquatchbackpack/cli.py
@@ -54,7 +54,7 @@ def main() -> None:
     type=int,
     show_default=True,
 )
-def search_usgs(
+def usgs_earthquake_data(
     duration: tuple[int, int],
     radius: int,
     coords: tuple[float, float],
@@ -64,6 +64,86 @@ def search_usgs(
     """Seaches USGS databases for relevant earthquake data and prints it
     to console
     """
+    issues = []
+
+    if radius > 5000:
+        issues.append(
+            f"Your provided radius ({radius}) is too large. The maximum"
+            + " is 5000."
+        )
+    elif radius <= 0:
+        issues.append(
+            f"Your provided radius ({radius}) is too small. The minimum"
+            + " is 0.1."
+        )
+
+    if coords[0] < -90.0:
+        issues.append(
+            f"Your provided latitude ({coords[0]}) is too low. The"
+            + " minimum is -90."
+        )
+    elif coords[0] > 90.0:
+        issues.append(
+            f"Your provided latitude ({coords[0]}) is too high. The"
+            + " maximum is 90."
+        )
+
+    if coords[1] < -180:
+        issues.append(
+            f"Your provided longitude ({coords[1]}) is too low. The"
+            + " minimum is -180."
+        )
+    elif coords[1] > 180:
+        issues.append(
+            f"Your provided longitude ({coords[1]}) is too high. The"
+            + " maximum is 180."
+        )
+
+    if min_magnitude < 0:
+        issues.append(
+            f"Your provided minimum magnitude ({min_magnitude}) is too"
+            + " small. The minimum is 0."
+        )
+    elif min_magnitude > 10:
+        issues.append(
+            f"Your provided minimum magnitude ({min_magnitude}) is too"
+            + "large. The maximum is 10."
+        )
+
+    if max_magnitude > 10:
+        issues.append(
+            f"Your provided maximum magnitude ({max_magnitude}) is too"
+            + " large. The maximum is 10."
+        )
+    elif max_magnitude < 0:
+        issues.append(
+            f"Your provided maximum magnitude ({max_magnitude}) is too"
+            + " small. The minimum is 0."
+        )
+
+    if min_magnitude > max_magnitude:
+        issues.append(
+            f"Your provided minimum magnitude ({min_magnitude}) cannot"
+            + " excede your provided maximum magnitude ({max_magnitude})."
+        )
+
+    if len(issues) > 0:
+        click.secho(
+            "Command Failed! One or more provided parameters is \
+            incorrect.\n",
+            fg="red",
+        )
+        if len(issues) == 1:
+            click.echo("ISSUE\n------")
+            click.echo(issues[0])
+            click.echo("------")
+        else:
+            click.echo("ISSUES\n------")
+            for issue in issues:
+                click.echo(issue)
+            click.echo("------")
+        return
+
     results = usgs.search_api(
         timedelta(duration[0], 0, 0, 0, 0, duration[1], 0),
         radius,
@@ -71,5 +151,16 @@ def search_usgs(
         min_magnitude,
         max_magnitude,
     )
-    for result in results:
-        print(result)
+
+    if len(results) > 0:
+        click.secho("SUCCESS!", fg="green")
+        click.echo("------")
+        for result in results:
+            click.echo(result)
+        click.echo("------")
+    else:
+        click.echo("No results found for the provided criteria :(")
+
+
+if __name__ == "__main__":
+    main()
