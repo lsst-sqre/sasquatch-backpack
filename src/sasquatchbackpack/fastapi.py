114880ac3fd936291e4b20d54448d4ca86e5edd4
Fireye <codekai16@gmail.com>
2025-11-21 16:59:01 -0700

    Functional FastAPI entrypoint for earthquake data

--- src/sasquatchbackpack/fastapi.py
+++ src/sasquatchbackpack/fastapi.py
@@ -1,1 +1,45 @@
 """Backpack FastAPI module."""
+
+from fastapi import FastAPI
+
+from sasquatchbackpack.sources.usgs.commands import usgs_earthquake_data
+
+app = FastAPI()
+
+
+@app.get(
+    "/sources/usgs/earthquake_data/{days}/{hours}/{radius}/{latitude}/{longitude}/{lower}/{upper}/{publish}/{method}"
+)
+async def read_item(
+    days: int,
+    hours: int,
+    radius: int,
+    latitude: int,
+    longitude: int,
+    lower: int,
+    upper: int,
+    publish: bool,  # noqa: FBT001
+    method: str,
+) -> None:
+    """Set earthquake_data command."""
+    args = [
+        "-d",
+        f"{days}",
+        f"{hours}",
+        "-r",
+        f"{radius}",
+        "-c",
+        f"{latitude}",
+        f"{longitude}",
+        "-m",
+        f"{lower}",
+        f"{upper}",
+        "-pm",
+        method,
+    ]
+
+    if publish:
+        args.append("--publish")
+
+    ctx = usgs_earthquake_data.make_context("usgs_earthquake_data", args)
+    usgs_earthquake_data.invoke(ctx)
