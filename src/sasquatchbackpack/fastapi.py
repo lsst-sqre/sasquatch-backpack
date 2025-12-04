946b650c198c9967c560a85c096eefd399c6cac9
Fireye <codekai16@gmail.com>
2025-12-04 14:23:00 -0700

    Polish Fastapi implementation     add substantive docstrings     add field defaults     add field descriptions     rename endpoint

--- src/sasquatchbackpack/fastapi.py
+++ src/sasquatchbackpack/fastapi.py
@@ -1,30 +1,71 @@
 """Backpack FastAPI module."""
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 from sasquatchbackpack.sources.usgs.commands import usgs_earthquake_data
 
 app = FastAPI()
 
 
-class EarthquakeParams(BaseModel):
-    """{days}/{hours}/{radius}/{latitude}/{longitude}/{lower}/{upper}/{publish}/{method}."""
+class EarthquakeRequest(BaseModel):
+    """Request body for USGS API earthquake query parameters."""
 
-    days: int
-    hours: int
-    radius: int
-    latitude: int
-    longitude: int
-    lower: int
-    upper: int
-    publish: bool
-    method: str
+    days: int = Field(
+        description="The number of days to look "
+        "back from the present when performing the search."
+    )
+    hours: int = Field(
+        description="The number of hours to look "
+        "back from the present when performing the search."
+    )
+
+    radius: int = Field(
+        default=400,
+        description="The radius around the provided coordinate to search.",
+    )
+
+    latitude: float = Field(
+        default=-30.22573200864174,
+        description="The latitude of the center coordinate",
+    )
+
+    longitude: float = Field(
+        default=-70.73932987127506,
+        description="The longitude of the center coordinate",
+    )
+
+    lower: int = Field(
+        default=2,
+        description="The lower bound for earthquake magnitude to search",
+    )
+
+    upper: int = Field(
+        default=10,
+        description="The upper bound for earthquake magnitude to search",
+    )
+
+    publish: bool = Field(
+        default=False,
+        description="Whether or not queried data should be published to"
+        "sasquatch",
+    )
+
+    method: str = Field(
+        default="DIRECT_CONNECTION",
+        description="The publish method for sending data to sasquatch",
+    )
 
 
-@app.post("/sources/usgs/earthquake_data/")
-async def read_item(params: EarthquakeParams) -> None:
-    """Set earthquake_data command."""
+@app.post("/sources/usgs/earthquake/")
+async def usgs_earthquake(params: EarthquakeRequest) -> None:
+    """Request to query Earthquake events from the USGS API.
+
+    Parameters
+    ----------
+    params: EarthquakeRequest
+        USGS Query Parameters to POST
+    """
     args = [
         "-d",
         f"{params.days}",
