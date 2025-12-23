d311ef9062257ca6bfd9bc20ec2572f6b8cba483
Fireye <codekai16@gmail.com>
2025-12-23 16:23:15 -0700

    Fix error handling

--- src/sasquatchbackpack/fastapi.py
+++ src/sasquatchbackpack/fastapi.py
@@ -1,5 +1,9 @@
 """Backpack FastAPI module."""
 
+from contextlib import redirect_stdout
+from io import StringIO
+
+import click
 from fastapi import FastAPI, Response
 
 from sasquatchbackpack.sources.usgs.commands import usgs_earthquake_data
@@ -38,9 +42,26 @@ async def usgs_earthquake(
     if params.publish:
         args.append("--publish")
 
-    ctx = usgs_earthquake_data.make_context("usgs_earthquake_data", args)
-    result = usgs_earthquake_data.invoke(ctx)
-    if result.exit_code != 0 or "Error" in result.output:
+    try:
+        ctx = usgs_earthquake_data.make_context("usgs_earthquake_data", args)
+
+        # capture click stdout for error handling
+        with redirect_stdout(StringIO()) as out:
+            usgs_earthquake_data.invoke(ctx)
+
+    except click.exceptions.BadParameter:
+        # improper input
         response.status_code = 400
-    elif "No results found" in result.output:
+        return
+
+    if "Error" in out.getvalue():
+        # api call/ post failed
+        response.status_code = 500
+
+    elif "No results found" in out.getvalue():
+        # api returned no results for query
+        response.status_code = 204
+
+    elif "publish mode disabled" in out.getvalue():
+        # call succeeded, and no data was added to sasquatch
         response.status_code = 200
